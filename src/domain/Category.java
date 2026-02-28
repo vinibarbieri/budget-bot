7a8bee15dba3abd706c684942397e643e8d90b50
vinibarbieri <vinibarbieri.dev@gmail.com>
2026-02-27 23:41:07 -0500

    feat: implement income and expense differentiation with ENUM CategoryType

--- src/domain/Category.java
+++ src/domain/Category.java
@@ -5,19 +5,23 @@ public class Category {
     private Long id;
     private Long userId;
     private String name;
+    private CategoryType type;
 
-    public Category(Long userId, String name) {
+    public Category(Long userId, String name, CategoryType type ) {
         this.userId = userId;
         this.name = name;
+        this.type = type;
     }
 
-    public Category(Long id, Long userId, String name){
+    public Category(Long id, Long userId, String name, CategoryType type){
         this.id = id;
         this.userId = userId;
         this.name = name;
+        this.type = type;
     }
 
     public Long getId() { return id; }
     public Long getUserId() { return userId; }
     public String getName() { return name; }
+    public CategoryType getType() { return type; }
 }
---
+++ src/domain/CategoryType.java
@@ -0,0 +1,6 @@
+package domain;
+
+public enum CategoryType {
+    INCOME,
+    EXPENSE
+}
